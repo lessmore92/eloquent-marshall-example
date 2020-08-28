502f3d36b5ab0932b96830b539edc596ea337532
Lessmore92 <bahramisoft@gmail.com>
2020-08-29 03:44:54 +0430

    basic models

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        "title",
+        'content',
+        'slug',
+        'featured_image',
+        'is_featured',
+        'is_published'
+    ];
+}
